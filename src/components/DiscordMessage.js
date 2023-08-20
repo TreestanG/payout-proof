bb8cb517281e463f1929d7c8efff469e0bce19e7
Koolwiza <tristangee@gmail.com>
2023-08-19 23:26:55 -0700

    add spoiler and fix hydration issue

--- src/components/DiscordMessage.js
+++ src/components/DiscordMessage.js
@@ -1,6 +1,25 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+
+const genLink = () => {
+    let length = 20
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return `https://discord.com/gifts/${result}`;
+}
 
 export default function DiscordMessage({ link, nitro, profile, name, content, time, second }) {
+
+    const [hydrated, setHydrated] = useState(false);
+    useEffect(() => {
+        setHydrated(true);
+    }, []);
+    if (!hydrated) return null
 
     let todaysDate = time
 
@@ -10,6 +29,12 @@ export default function DiscordMessage({ link, nitro, profile, name, content, ti
     const hour12 = hour % 12 || 12
 
     if (minute < 10) minute = "0" + minute.toString()
+
+    let nLink = content
+    if (content === "nitro") {
+        nLink = link ? genLink() : "nitro"
+    }
+
 
     return (
         <div className=" flex pt-4">
@@ -21,7 +46,13 @@ export default function DiscordMessage({ link, nitro, profile, name, content, ti
                     <p className="text-md font-medium">{name}</p>
                     <p className='text-[.75rem] text-[#6e727a] font-[400]'>Today at {hour12}:{minute} {ampm}</p>
                 </div>
-                <p className={link ? "text-[#00a8fc]" : ""}>{content}</p>
+                <p className={link ? "text-[#00a8fc]" : ""}>{nLink}</p>
+                {
+                    content === "nitro" && (
+                        <div className={"h-6 w-60 bg-black absolute -translate-y-6 translate-x-44"}>
+                        </div>
+                    )
+                }
 
                 {
                     nitro ? (
@@ -31,9 +62,9 @@ export default function DiscordMessage({ link, nitro, profile, name, content, ti
                                 <p className='py-2 pb-[68px] text-[#b7b8ba]'>
                                     <span className="font-[700] ">{nitro.payer}</span> has gifted you Nitro for <span className="font-[700]">1 {nitro.amount}</span>!
                                 </p>
-                                <div className='flex justify-between items-end'>
+                                <div className='flex justify-between items-end w-[350px]'>
                                     <button className="py-2 px-5 rounded-md bg-[#42b681] font-[450]">Accept</button>
-                                    <p className='text-[#6e727a] text-xs font-medium translate-x-12'>Expires in 47 hours</p>
+                                    <p className='text-[#6e727a] text-xs font-medium '>Expires in 47 hours</p>
                                 </div>
                             </div>
                             <img src="nitro.png" className='h-[190px] rounded-r-lg '></img>
