9214134c84403f58af3b1e4ed865a83d2b2e83ac
Koolwiza <tristangee@gmail.com>
2023-08-19 23:28:48 -0700

    clean up + AI

--- src/pages/roblox.js
+++ src/pages/roblox.js
@@ -28,7 +28,7 @@ export default function Home() {
     })
 
     const handleChange = (name, value) => {
-        if (name === "amount") value = parseInt(value).toLocaleString()
+        if (name === "amount" && value) value = parseInt(value).toLocaleString()
         if (name === "payer" && value.length > 20) value = value.slice(0, 20) + "..."
         if (name === "username" && value.split(",").length > 1) value = value.split(",")
 
@@ -37,8 +37,6 @@ export default function Home() {
             [name]: value
         })
     }
-
-    console.log(values.username)
 
     const getImage = async (name) => {
         const id = await getUserID(name)
@@ -54,13 +52,21 @@ export default function Home() {
         })
     }
 
+    const aiGenerated = () => {
+        fetch('/api/username').then(res => res.json()).then(data => {
+            setValues({
+                ...values,
+                username: data.username,
+            })
+        })
+    }
+
     return (
-        <main className={"flex items-center flex-col"}>
+        <main className={"flex items-center flex-col bg-black text-white h-screen pt-8"}>
             <div className={font.className}>
                 <h1 className="text-5xl">Roblox Payout Proof Generator</h1>
                 <p className="p-8 text-lg">Generate a proof of payment for your Roblox group.</p>
 
-                <Image src="/robux.png" width="50" height="50" alt="robux" />
 
                 <form>
                     <input onChange={event => {
@@ -73,19 +79,28 @@ export default function Home() {
                     <input onChange={event => {
                         handleChange("amount", event.target.value)
                     }} className="border-2 text-black border-black rounded-md" type="text" placeholder="Amount" />
+                    <button className="font-medium underline mb-8" onClick={() => getImage(values.payer)}>Get Image</button>
+
                 </form>
 
-                <button className="font-medium underline mb-8" onClick={() => getImage(values.payer)}>Get Image</button>
+                <button className="rounded-lg px-6 py-2 mb-8 font-medium text-gray-100 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600" onClick={aiGenerated}>Generate email with AI</button>
+
+
                 {
                     (Array.isArray(values.username) && values.username.length > 0) ? values.username.map((name, i) => {
                         return (
                             <RobuxPayout key={i} values={{
                                 ...values,
                                 username: name,
-                            }} index={i}/>
+                            }} index={i} />
                         )
                     }) : <RobuxPayout values={values} />
                 }
+
+
+                <div>
+
+                </div>
             </div>
 
         </main>
